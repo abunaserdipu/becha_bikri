742d372e608c00880d20b25e2ed5dc726ad96ca6
abunaserdipu <abunaserdipu0@gmail.com>
2022-11-28 18:18:37 +0600

    solved  database connection

--- backend/api/products/addproducts.php
+++ backend/api/products/addproducts.php
@@ -1,2 +1,10 @@
 <?php
 include("./api/db_connection.php");
+
+$productlist = mysqli_query($db_conn, "SELECT * FROM products");
+
+if (mysqli_num_rows($productlist) > 0) {
+    while ($row = mysqli_fetch_assoc($productlist)) {
+        print_r($row);
+    }
+}
--- backend/api/products/productlist.php
+++ backend/api/products/productlist.php
@@ -1,14 +1,15 @@
 <?php
-include('./api/db_connection.php');
+include('../db_connection.php');
 
 $productList = mysqli_query($db_conn, "SELECT * FROM products");
 if (mysqli_num_rows($productList) > 0) {
     while ($row = mysqli_fetch_assoc($productList)) {
-        $viewjson['name'] = $row['name'];
-        $viewjson['price'] = $row['price'];
-        $viewjson['details'] = $row['details'];
+        // $viewjson['name'] = $row['name'];
+        // $viewjson['price'] = $row['price'];
+        // $viewjson['details'] = $row['details'];
 
-        $jsonarray['productData'][] = $viewjson;
+        // $jsonarray['productData'][] = $viewjson;
+        print_r($row);
     }
-    echo json_encode($jsonarray);
+    // echo json_encode($jsonarray);
 }
